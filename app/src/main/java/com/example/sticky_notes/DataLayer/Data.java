91e37f3e6642019f4fbb7b2732dbac983532e59a
jaspreetsidhu3 <jaspreetsidhu3may@gmail.com>
2021-10-28 17:14:35 +0530

    Added DataLayer and MainActivity design

---
+++ app/src/main/java/com/example/sticky_notes/DataLayer/Data.java
@@ -0,0 +1,50 @@
+package com.example.sticky_notes.DataLayer;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.nio.charset.StandardCharsets;
+
+public class Data {
+    Context _context;
+
+    public Data(Context context) {
+        _context = context;
+    }
+
+    public String getNotes() {
+        File file = new File(_context.getFilesDir().getPath() + "/notes.txt");
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append("\n");
+            }
+            br.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+
+    public void setNotes(String note) throws IOException {
+        FileOutputStream fos = null;
+        try {
+            fos = _context.getApplicationContext().openFileOutput("notes.txt", Context.MODE_APPEND);
+            fos.write(note.getBytes());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (fos != null) {
+                fos.close();
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/sticky_notes/MainActivity.java
+++ app/src/main/java/com/example/sticky_notes/MainActivity.java
@@ -3,13 +3,32 @@ package com.example.sticky_notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.sticky_notes.DataLayer.Data;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtNote;
+    private Button btnSave;
+    private Data data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtNote = findViewById(R.id.edtNote);
+        btnSave = findViewById(R.id.save);
+        data = new Data(getApplicationContext());
+
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
 
