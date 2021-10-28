03798728769c019a23434b2f98f2ed4447f22e2a
jaspreetsidhu3 <jaspreetsidhu3may@gmail.com>
2021-10-28 17:40:04 +0530

    Completed App functionality

--- app/src/main/java/com/example/sticky_notes/MainActivity.java
+++ app/src/main/java/com/example/sticky_notes/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.sticky_notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import com.example.sticky_notes.DataLayer.Data;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private EditText edtNote;
@@ -26,7 +33,20 @@ public class MainActivity extends AppCompatActivity {
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (TextUtils.isEmpty(edtNote.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "Please enter note", Toast.LENGTH_LONG).show();
+                }
+                try {
+                    data.setNotes(edtNote.getText().toString());
+                    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(getApplicationContext());
+                    RemoteViews remoteViews = new RemoteViews(getApplicationContext().getPackageName(), R.layout.widget_layout);
+                    ComponentName widget = new ComponentName(getApplicationContext(), AppWidget.class);
+                    remoteViews.setTextViewText(R.id.idTVWidget, edtNote.getText().toString());
+                    appWidgetManager.updateAppWidget(widget, remoteViews);
+                    Toast.makeText(getApplicationContext(), "Updated", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
